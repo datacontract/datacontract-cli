c0e3d96a266c8543ad7ad7fbe75356eaf0acc758
Stefan Negele <stefan.negele@innoq.com>
2023-07-24 16:53:39 +0200

    Make optional fields of data contract pointers

--- datacontract.go
+++ datacontract.go
@@ -4,37 +4,37 @@ type DataContract struct {
 	DataContractSpecification string
 	Info                      struct {
 		Id        string
-		Purpose   string
-		Status    string // Typical values are: draft, requested, approved, rejected, canceled
-		StartDate string
-		EndDate   string
+		Purpose   *string
+		Status    *string // Typical values are: draft, requested, approved, rejected, canceled
+		StartDate *string
+		EndDate   *string
 	}
 	Provider struct {
 		TeamId          string
-		TeamName        string
+		TeamName        *string
 		DataProductId   string
-		DataProductName string
+		DataProductName *string
 		OutputPortId    string
-		OutputPortName  string
+		OutputPortName  *string
 	}
 	Consumer struct {
 		TeamId          string
-		TeamName        string
-		DataProductId   string
-		DataProductName string
+		TeamName        *string
+		DataProductId   *string
+		DataProductName *string
 	}
 	Terms struct {
-		Usage                string
-		Limitations          string
-		Billing              string
-		NoticePeriod         string // ISO-8601
-		IndividualAgreements string
+		Usage                *string
+		Limitations          *string
+		Billing              *string
+		NoticePeriod         *string // ISO-8601
+		IndividualAgreements *string
 	}
-	Schema struct {
+	Schema *struct {
 		Type          string // Typical values are: dbt, bigquery, jsonschema, openapi, protobuf, paypal, custom
 		Specification string
 	}
-	Tags   []string
-	Links  map[string]string
-	Custom map[string]string
+	Tags   *[]string
+	Links  *map[string]string
+	Custom *map[string]string
 }
