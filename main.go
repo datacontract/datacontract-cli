a1179e16317f575231b8de36314e23fd49b8bfec
Stefan Negele <stefan.negele@innoq.com>
2023-09-26 17:27:53 +0200

    Update version

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	app := &cli.App{
 		Name:    "datacontract",
 		Usage:   "Manage your data contracts 📄",
-		Version: "v0.1.1",
+		Version: "v0.2.0",
 		Authors: []*cli.Author{
 			{Name: "Stefan Negele", Email: "stefan.negele@innoq.com"},
 		},
