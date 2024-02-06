8bd22156927cd97d34053133f101a7bfeb46368a
Marian Grigoras <marian.grigoras@innoq.com>
2024-02-06 16:04:59 +0200

    Disable test from running on the build pipeline

--- tests/test_examples_bigquery.py
+++ tests/test_examples_bigquery.py
@@ -9,12 +9,16 @@ logging.basicConfig(level=logging.INFO, force=True)
 
 datacontract = "examples/bigquery/datacontract.yaml"
 
-@pytest.mark.skipif(os.environ.get("DATACONTRACT_BIGQUERY_ACCOUNT_INFO_JSON_PATH") is None, reason="Requires DATACONTRACT_BIGQUERY_ACCOUNT_INFO_JSON_PATH to be set")
-def test_examples_bigquery():
-    data_contract = DataContract(data_contract_file=datacontract)
+# Deactivated because the test requires special setup on a non-free BigQuery account.
+# Can activate for testing locally, using a custom account_info file.
+# For the provided datacontract.yaml the data file from s3-csv should be imported in the target BigQuery table.
 
-    run = data_contract.test()
-
-    print(run)
-    assert run.result == "passed"
-    assert all(check.result == "passed" for check in run.checks)
+# @pytest.mark.skipif(os.environ.get("DATACONTRACT_BIGQUERY_ACCOUNT_INFO_JSON_PATH") is None, reason="Requires DATACONTRACT_BIGQUERY_ACCOUNT_INFO_JSON_PATH to be set")
+# def test_examples_bigquery():
+#     data_contract = DataContract(data_contract_file=datacontract)
+#
+#     run = data_contract.test()
+#
+#     print(run)
+#     assert run.result == "passed"
+#     assert all(check.result == "passed" for check in run.checks)
