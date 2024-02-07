3ff64b1a2b1a92a2dbcc2f961f4371d7ec3b64f2
Jan Seeger <jan.seeger@thenybble.de>
2024-02-07 15:45:20 +0100

    Implemented model/example linting, added tests for CSV.
    Examples are now linted against the model's fields. Implementation is tested slightly for "flat" CSV
    examples, more tests to come.

--- datacontract/lint/linters.py
+++ datacontract/lint/linters.py
@@ -1,4 +1,11 @@
+import csv
+import yaml
+import json
+import io
+
 from .result import LintingResult
+from ..model.data_contract_specification import DataContractSpecification, Example
+
 """This module contains linter definitions for linting a data contract.
 
 Lints are quality checks that can succeed, fail, or warn. They are
@@ -8,15 +15,50 @@ ignored, and are high-level requirements on the format of a data
 contract."""
 
 
-def linter_example_schema(data_contract_yaml) -> LintingResult:
+def get_example_headers(example: Example) -> list[str]:
+    match example.type:
+        case "csv":
+            dialect = csv.Sniffer().sniff(example.data)
+            data = io.StringIO(example.data)
+            reader = csv.reader(data, dialect=dialect)
+            return next(reader)
+        case "yaml":
+            data = yaml.safe_load(example.data)
+            return data.keys()
+        case "json":
+            data = json.loads(example.data)
+            return data.keys()
+
+
+def linter_example_schema(data_contract_yaml: DataContractSpecification) -> LintingResult:
     """Check whether the example(s) match the model."""
     result = LintingResult("Example(s) match schema")
     examples = data_contract_yaml.examples
     models = data_contract_yaml.models
+    examples_with_model = []
     for (index, example) in enumerate(examples):
         if example.model not in models:
             result = result.with_error(
                 f"Example {index + 1} has non-existent model '{example.model}'")
+        else:
+            examples_with_model.append(
+                (index, example, models.get(example.model)))
+    for (index, example, model) in examples_with_model:
+        if example.type == "custom":
+            result = result.with_warning(f"Example {index + 1} has type"
+                                         " \"custom\", cannot check model"
+                                         " conformance")
+        else:
+            headers = get_example_headers(example)
+            for example_header in headers:
+                if example_header not in model.fields:
+                    result = result.with_error(f"Example {index + 1} has field '{example_header}'"
+                                               f" that's not contained in model '{example.model}'")
+            for (field_name, field_value) in model.fields.items():
+                if field_name not in headers and field_value.required:
+                    result = result.with_error(
+                        f"Example {index + 1} is missing field '{field_name}'"
+                        f" required by model '{example.model}'")
     return result
 
 
--- tests/test_linters.py
+++ tests/test_linters.py
@@ -1,13 +1,56 @@
 import datacontract.lint.linters as linters
 import datacontract.lint.resolve as resolve
 import datacontract.lint.result as lint_result
+import datacontract.model.data_contract_specification as spec
+
 
 def test_lint_invalid_model():
     data_contract_file = "examples/lint/datacontract_unknown_model.yaml"
-    spec = resolve.resolve_data_contract_from_location(data_contract_file)
-    result = linters.linter_example_schema(spec)
-    expected = lint_result.LinterResult.error("Example 1 has non-existent model 'orders'")
-    assert not result.no_errors_or_warnings()
+    contract = resolve.resolve_data_contract_from_location(data_contract_file)
+    result = linters.linter_example_schema(contract)
+    expected = lint_result.LinterResult.error(
+        "Example 1 has non-existent model 'orders'")
     assert result.error_results() == [
         expected
-        ]
+    ]
+    assert result.warning_results() == []
+
+
+def test_lint_valid_csv_columns():
+    contract = resolve.resolve_data_contract_from_location(
+        "examples/lint/datacontract_csv_lint_base.yaml")
+    result = linters.linter_example_schema(contract)
+    assert result.no_errors_or_warnings()
+
+
+def test_lint_extra_model_columns():
+    base_spec = resolve.resolve_data_contract_from_location(
+        "examples/lint/datacontract_csv_lint_base.yaml")
+    base_spec.models['orders'] = spec.Model(fields={
+        "column_1": spec.Field(type="str"),
+        "column_2": spec.Field(type="str"),
+        "column_3": spec.Field(type="str", required=True)
+    })
+    result = linters.linter_example_schema(base_spec)
+    expected = lint_result.LinterResult.error(
+        "Example 1 is missing field 'column_3' required by model 'orders'")
+    assert result.error_results() == [
+        expected
+    ]
+    assert result.warning_results() == []
+
+
+def test_lint_extra_example_columns():
+    base_spec = resolve.resolve_data_contract_from_location(
+        "examples/lint/datacontract_csv_lint_base.yaml")
+    base_spec.examples[0] = spec.Example(
+        type="csv",
+        model="orders",
+        data="column_1, column_2, column_3\nvalue_1, value_2, value_3")
+    result = linters.linter_example_schema(base_spec)
+    expected = lint_result.LinterResult.error(
+        "Example 1 has field 'column_3' that's not contained in model 'orders'")
+    assert result.error_results() == [
+        expected
+    ]
+    assert result.warning_results() == []
