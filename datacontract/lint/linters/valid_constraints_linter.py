50b197cd233085bc37ee7a6ba3a60de949837088
Jan Seeger <jan.seeger@thenybble.de>
2024-03-06 16:18:05 +0100

    Extended field constraints linter.
    Field constraints are now checked whether they make sense.

--- datacontract/lint/linters/valid_constraints_linter.py
+++ datacontract/lint/linters/valid_constraints_linter.py
@@ -1,13 +1,18 @@
 from ..lint import Linter, LinterResult
 from datacontract.model.data_contract_specification import\
-    DataContractSpecification, Model
+    DataContractSpecification, Field
 
 
 class ValidFieldConstraintsLinter(Linter):
     """Check validity of field constraints.
 
-      More precisely, check that only numeric constraints are specified on fields
-      of numeric type and string constraints on fields of string type.
+      More precisely, check that only numeric constraints are specified on
+      fields of numeric type and string constraints on fields of string type.
+      Additionally, the linter checks that defined constraints make sense.
+      Minimum values should not be greater than maximum values, exclusive and
+      non-exclusive minimum and maximum should not be combined and string
+      pattern and format should not be combined.
+
     """
 
     valid_types_for_constraint = {
@@ -24,6 +29,42 @@ class ValidFieldConstraintsLinter(Linter):
         "exclusiveMaximum": set(["int", "integer", "number", "decimal", "numeric",
                                  "long", "bigint", "float", "double"]),
     }
+
+    def check_minimum_maximum(self, field: Field, field_name: str, model_name: str) -> LinterResult:
+        match (field.minimum, field.maximum, field.exclusiveMinimum, field.exclusiveMaximum):
+            case (_min, _max, None, None) if _min > _max:
+                return LinterResult.erroneous(
+                    f"Minimum {_min} is greater than maximum {_max} on "
+                    f"field '{field_name}' in model '{model_name}'.")
+            case (None, None, _min, _max) if _min >= max:
+                return LinterResult.erroneous(
+                    f"Exclusive minimum {_min} is greater than exclusive"
+                    f" maximum {_max} on field '{field_name}' in model '{model_name}'.")
+            case (_min, _max, _xmin, _xmax):
+                return LinterResult.erroneous(
+                    f"Both exclusive minimum and exclusive maximum are "
+                    f"defined on field '{field_name}' in model '{model_name}'.")
+            case (_min, None, _xmin, None):
+                return LinterResult.erroneous(
+                    f"Both exclusive and non-exclusive minimum are "
+                    f"defined on field '{field_name}' in model '{model_name}'.")
+            case (None, _max, None, _xmax):
+                return LinterResult.erroneous(
+                    f"Both exclusive and non-exclusive minimum are "
+                    f"defined on field '{field_name}' in model '{model_name}'.")
+        return LinterResult()
+
+    def check_string_constraints(self, field: Field, field_name: str, model_name: str) -> LinterResult:
+        result = LinterResult()
+        if field.minLength and field.maxLength and field.minLength > field.maxLength:
+            result = result.with_error(
+                f"Minimum length is greater that maximum length on"
+                f" field '{field_name}' in model '{model_name}'.")
+        if field.pattern and field.format:
+            result = result.with_error(
+                f"Both a pattern and a format are defined for field"
+                f" '{field_name}' in model '{model_name}'.")
+        return result
 
     @property
     def name(self):
@@ -42,4 +83,6 @@ class ValidFieldConstraintsLinter(Linter):
                             f"Forbidden constraint '{_property}' defined on field "
                             f"'{field_name}' in model '{model_name}'. Field type "
                             f"is '{field.type}'.")
+                result = result.combine(self.check_minimum_maximum(field, field_name, model_name))
+                result = result.combine(self.check_string_constraints(field, field_name, model_name))
         return result
