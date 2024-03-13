681868b419b7e9565bab99540af23f241c013b71
Simon Harrer <simon.harrer@gmail.com>
2024-03-13 10:41:44 +0100

    Tests for missing examples

---
+++ tests/test_examples_examples_missing.py
@@ -0,0 +1,25 @@
+import logging
+
+from typer.testing import CliRunner
+
+from datacontract.cli import app
+from datacontract.data_contract import DataContract
+
+runner = CliRunner()
+
+logging.basicConfig(level=logging.DEBUG, force=True)
+
+
+def test_cli():
+    result = runner.invoke(app, ["test", "--examples", "./examples/examples/datacontract_missing.yaml"])
+    assert result.exit_code == 1
+
+
+def test_missing():
+    data_contract = DataContract(data_contract_file="examples/examples/datacontract_missing.yaml", examples=True)
+    run = data_contract.test()
+    print(run)
+    print(run.result)
+    assert run.result == "warning"
+
+
