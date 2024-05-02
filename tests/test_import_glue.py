3a46c37dab0580aced843b217d2ba55ecadec6be
Jacob Verhoeks <jjverhoeks@schubergphilis.com>
2024-05-02 09:46:39 +0200

    Move moto test to fixtures

--- tests/test_import_glue.py
+++ tests/test_import_glue.py
@@ -4,14 +4,19 @@ import logging
 import yaml
 from moto import mock_aws
 import os
+import pytest
 
 from datacontract.cli import app
 from datacontract.data_contract import DataContract
 
 logging.basicConfig(level=logging.INFO, force=True)
 
+db_name = "test_database"
+table_name = "test_table"
 
-def setup_mock_glue(db_name="test_database", table_name="test_table"):
+
+@pytest.fixture(scope="function")
+def aws_credentials():
     """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
@@ -19,49 +24,52 @@ def setup_mock_glue(db_name="test_database", table_name="test_table"):
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 
-    client = boto3.client("glue")
 
-    client.create_database(
-        DatabaseInput={
-            "Name": db_name,
-        },
-    )
+@pytest.fixture(scope="function")
+def setup_mock_glue(aws_credentials):
+    with mock_aws():
+        client = boto3.client("glue")
 
-    client.create_table(
-        DatabaseName=db_name,
-        TableInput={
-            "Name": table_name,
-            "StorageDescriptor": {
-                "Columns": [
+        client.create_database(
+            DatabaseInput={
+                "Name": db_name,
+            },
+        )
+
+        client.create_table(
+            DatabaseName=db_name,
+            TableInput={
+                "Name": table_name,
+                "StorageDescriptor": {
+                    "Columns": [
+                        {
+                            "Name": "field_one",
+                            "Type": "string",
+                        },
+                        {
+                            "Name": "field_two",
+                            "Type": "integer",
+                        },
+                        {
+                            "Name": "field_three",
+                            "Type": "timestamp",
+                        },
+                    ]
+                },
+                "PartitionKeys": [
                     {
-                        "Name": "field_one",
+                        "Name": "part_one",
                         "Type": "string",
                     },
-                    {
-                        "Name": "field_two",
-                        "Type": "integer",
-                    },
-                    {
-                        "Name": "field_three",
-                        "Type": "timestamp",
-                    },
-                ]
+                ],
             },
-            "PartitionKeys": [
-                {
-                    "Name": "part_one",
-                    "Type": "string",
-                },
-            ],
-        },
-    )
+        )
+        # everything after the yield will run after the fixture is used
+        yield client
 
 
-def test_cli():
-    mock = mock_aws()
-    mock.start()
-    setup_mock_glue()
-
+@mock_aws
+def test_cli(setup_mock_glue):
     runner = CliRunner()
     result = runner.invoke(
         app,
@@ -74,14 +82,10 @@ def test_cli():
         ],
     )
     assert result.exit_code == 0
-    mock.stop()
 
 
-def test_import_glue_schema():
-    mock = mock_aws()
-    mock.start()
-    setup_mock_glue()
-
+@mock_aws
+def test_import_glue_schema(setup_mock_glue):
     result = DataContract().import_from_source("glue", "test_database")
 
     with open("fixtures/glue/datacontract.yaml") as file:
@@ -91,4 +95,3 @@ def test_import_glue_schema():
     assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
     # Disable linters so we don't get "missing description" warnings
     assert DataContract(data_contract_str=expected).lint(enabled_linters=set()).has_passed()
-    mock.stop()
