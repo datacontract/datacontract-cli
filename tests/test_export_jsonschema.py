94ac626e2624f38fa5b7948a6539b44405f63f4d
nyeongna <jin03114@gmail.com>
2024-05-07 17:59:32 +0900

    fix(tests/test_export_jsonschema): roll-back what was unintentionally deleted

--- tests/test_export_jsonschema.py
+++ tests/test_export_jsonschema.py
@@ -114,6 +114,60 @@ def test_to_jsonschemas():
 
 
 def test_to_jsonschemas_complex():
+    data_contract_file = "fixtures/s3-json-complex/datacontract.yaml"
+    file_content = read_file(data_contract_file=data_contract_file)
+    data_contract = DataContractSpecification.from_string(file_content)
+    expected_json_schema = """{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+      "specversion": {
+        "type": "string"
+      },
+      "type": {
+        "type": "string"
+      },
+      "source": {
+        "type": "string",
+        "format": "uri"
+      },
+      "id": {
+        "type": "string"
+      },
+      "time": {
+        "type": "string",
+        "format": "date-time"
+      },
+      "subject": {
+        "type": ["string", "null"]
+      },
+      "data": {
+        "type": ["object", "null"],
+        "properties": {
+          "sku": {
+            "type": "string"
+          },
+          "updated": {
+            "type": "string",
+            "format": "date-time"
+          },
+          "quantity": {
+            "type": "integer"
+          }
+        },
+        "required": ["sku", "updated", "quantity"]
+      }
+    },
+    "required": ["specversion", "type", "source", "id", "time"]
+}
+"""
+
+    result = to_jsonschemas(data_contract)
+
+    assert result["inventory"] == json.loads(expected_json_schema)
+
+
+def test_to_jsonschemas_complex_2():
     data_contract_file = "fixtures/local-json-complex/datacontract.yaml"
     file_content = read_file(data_contract_file=data_contract_file)
     data_contract = DataContractSpecification.from_string(file_content)
