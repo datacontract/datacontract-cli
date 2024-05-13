d8b12e3685dab1592e639d5a99767bcd1a095baa
Joachim Praetorius <joachim.praetorius@innoq.com>
2024-05-13 17:21:07 +0200

    Add tests

---
+++ tests/test_import_bigquery.py
@@ -0,0 +1,34 @@
+import logging
+
+import yaml
+from typer.testing import CliRunner
+
+from datacontract.cli import app
+from datacontract.data_contract import DataContract
+
+logging.basicConfig(level=logging.DEBUG, force=True)
+
+
+def test_cli():
+    runner = CliRunner()
+    result = runner.invoke(
+        app,
+        [
+            "import",
+            "--format",
+            "bigquery",
+            "--source",
+            "fixtures/bigquery/import/complete_table_schema.json",
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_import_bigquery_schema():
+    result = DataContract().import_from_source("bigquery", "fixtures/bigquery/import/complete_table_schema.json")
+
+    print("Result:\n", result.to_yaml())
+    with open("fixtures/bigquery/import/datacontract.yaml") as file:
+        expected = file.read()
+    assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
+    assert DataContract(data_contract_str=expected).lint(enabled_linters="none").has_passed()
