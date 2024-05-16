ec8b5ee3694c94cea1745754237174fc878f3e33
Joachim Praetorius <joachim.praetorius@innoq.com>
2024-05-16 10:22:49 +0200

    Update definitions
    - move them to their opwn partial
    - move enum into the content column
    - try to highlight the different optional aspects a tad

--- datacontract/templates/style/tailwind.config.js
+++ datacontract/templates/style/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["../datacontract.html", "../index.html", "../partials/model_field.html"],
+  content: ["../datacontract.html", "../index.html", "../partials/model_field.html", "../partials/definition.html"],
   theme: { },
   plugins: [],
 }
