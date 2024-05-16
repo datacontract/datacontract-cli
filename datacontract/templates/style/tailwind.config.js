d4c1a4d3826049de5f627705749b99237da8dee2
Joachim Praetorius <joachim.praetorius@innoq.com>
2024-05-16 10:13:49 +0200

    Handle Model Fields and their nesting through partials
    - added jinja partials as dependency
    - extracted the model and the nesting handling out to its own partial

--- datacontract/templates/style/tailwind.config.js
+++ datacontract/templates/style/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["../datacontract.html", "../index.html"],
+  content: ["../datacontract.html", "../index.html", "../partials/model_field.html"],
   theme: { },
   plugins: [],
 }
