4ceda66832ab9bcce54c97f2bb9b0e1aa3733da3
Dennis Stachowicz <freeboard@posteo.net>
2024-06-04 19:40:00 +0200

    Import a single table from databricks unity catalog (via file or API); add tests

---
+++ tests/test_import_unity_file.py
@@ -0,0 +1,36 @@
+import logging
+
+import yaml
+from typer.testing import CliRunner
+
+from datacontract.cli import app
+from datacontract.data_contract import DataContract
+
+logging.basicConfig(level=logging.DEBUG, force=True)
+
+def test_cli():
+    print("running test_cli")
+    runner = CliRunner()
+    result = runner.invoke(
+        app,
+        [
+            "import",
+            "--format",
+            "unity",
+            "--source",
+            "fixtures/databricks-unity/import/unity_table_schema.json",
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_import_unity():
+    print("running test_import_unity")
+    result = DataContract().import_from_source("unity", "fixtures/databricks-unity/import/unity_table_schema.json")
+
+    with open("fixtures/databricks-unity/import/datacontract.yaml") as file:
+        expected = file.read()
+
+    print("Result:\n", result.to_yaml())
+    assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
+    assert DataContract(data_contract_str=expected).lint(enabled_linters="none").has_passed()
