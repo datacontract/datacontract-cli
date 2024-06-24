da0857afdb0a3ca603606dfd5554cb2112ca5ac6
Rodrigo Carneiro <teoria@gmail.com>
2024-06-24 17:15:38 -0300

    fix: validate dependencies before add to the importer factory (#288)
    Co-authored-by: Carneiro, Rodrigo <Rodrigo.Carneiro@br.experian.com>

--- datacontract/imports/importer_factory.py
+++ datacontract/imports/importer_factory.py
@@ -1,11 +1,5 @@
-from datacontract.imports.avro_importer import AvroImporter
-from datacontract.imports.bigquery_importer import BigQueryImporter
-from datacontract.imports.glue_importer import GlueImporter
+import importlib.util
 from datacontract.imports.importer import ImportFormat, Importer
-from datacontract.imports.jsonschema_importer import JsonSchemaImporter
-from datacontract.imports.odcs_importer import OdcsImporter
-from datacontract.imports.sql_importer import SqlImporter
-from datacontract.imports.unity_importer import UnityImporter
 
 
 class ImporterFactory:
@@ -22,10 +16,40 @@ class ImporterFactory:
 
 
 importer_factory = ImporterFactory()
-importer_factory.register_importer(ImportFormat.avro, AvroImporter)
-importer_factory.register_importer(ImportFormat.bigquery, BigQueryImporter)
-importer_factory.register_importer(ImportFormat.glue, GlueImporter)
-importer_factory.register_importer(ImportFormat.jsonschema, JsonSchemaImporter)
-importer_factory.register_importer(ImportFormat.odcs, OdcsImporter)
-importer_factory.register_importer(ImportFormat.sql, SqlImporter)
-importer_factory.register_importer(ImportFormat.unity, UnityImporter)
+
+if importlib.util.find_spec("datacontract.imports.avro_importer"):
+    from datacontract.imports.avro_importer import AvroImporter
+
+    importer_factory.register_importer(ImportFormat.avro, AvroImporter)
+
+if importlib.util.find_spec("datacontract.imports.bigquery_importer"):
+    from datacontract.imports.bigquery_importer import BigQueryImporter
+
+    importer_factory.register_importer(ImportFormat.bigquery, BigQueryImporter)
+
+if importlib.util.find_spec("datacontract.imports.glue_importer"):
+    from datacontract.imports.glue_importer import GlueImporter
+
+    importer_factory.register_importer(ImportFormat.glue, GlueImporter)
+
+if importlib.util.find_spec("datacontract.imports.jsonschema_importer"):
+    from datacontract.imports.jsonschema_importer import JsonSchemaImporter
+
+    importer_factory.register_importer(ImportFormat.jsonschema, JsonSchemaImporter)
+
+if importlib.util.find_spec("datacontract.imports.odcs_importer"):
+    from datacontract.imports.odcs_importer import OdcsImporter
+
+    importer_factory.register_importer(ImportFormat.odcs, OdcsImporter)
+
+
+if importlib.util.find_spec("datacontract.imports.sql_importer"):
+    from datacontract.imports.sql_importer import SqlImporter
+
+    importer_factory.register_importer(ImportFormat.sql, SqlImporter)
+
+
+if importlib.util.find_spec("datacontract.imports.sql_importer"):
+    from datacontract.imports.unity_importer import UnityImporter
+
+    importer_factory.register_importer(ImportFormat.unity, UnityImporter)
