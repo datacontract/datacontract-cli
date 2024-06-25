c98e3ed132369fabf32d278871e0da8e4d57f073
Pierre Monnet <pierre.monnet@decathlon.com>
2024-06-25 14:28:23 +0200

    fix: import problem from importer factory #286

--- datacontract/imports/importer_factory.py
+++ datacontract/imports/importer_factory.py
@@ -1,55 +1,37 @@
-import importlib.util
-from datacontract.imports.importer import ImportFormat, Importer
+from datacontract.imports.importer import Importer
 
 
 class ImporterFactory:
     def __init__(self):
         self.dict_importer = {}
 
-    def register_importer(self, name, exporter):
-        self.dict_importer.update({name: exporter})
-
     def create(self, name) -> Importer:
-        if name not in self.dict_importer.keys():
-            raise ValueError(f"Import format {name} not supported.")
-        return self.dict_importer[name](name)
+        if name == "avro":
+            from datacontract.imports.avro_importer import AvroImporter
 
+            return AvroImporter
+        if name == "bigquery":
+            from datacontract.imports.bigquery_importer import BigQueryImporter
 
-importer_factory = ImporterFactory()
+            return BigQueryImporter
+        if name == "glue":
+            from datacontract.imports.glue_importer import GlueImporter
 
-if importlib.util.find_spec("datacontract.imports.avro_importer"):
-    from datacontract.imports.avro_importer import AvroImporter
+            return GlueImporter
+        if name == "jsonschema":
+            from datacontract.imports.jsonschema_importer import JsonSchemaImporter
 
-    importer_factory.register_importer(ImportFormat.avro, AvroImporter)
+            return JsonSchemaImporter
+        if name == "odcs":
+            from datacontract.imports.odcs_importer import OdcsImporter
 
-if importlib.util.find_spec("datacontract.imports.bigquery_importer"):
-    from datacontract.imports.bigquery_importer import BigQueryImporter
+            return OdcsImporter
+        if name == "sql":
+            from datacontract.imports.sql_importer import SqlImporter
 
-    importer_factory.register_importer(ImportFormat.bigquery, BigQueryImporter)
+            return SqlImporter
+        if name == "unity":
+            from datacontract.imports.unity_importer import UnityImporter
 
-if importlib.util.find_spec("datacontract.imports.glue_importer"):
-    from datacontract.imports.glue_importer import GlueImporter
-
-    importer_factory.register_importer(ImportFormat.glue, GlueImporter)
-
-if importlib.util.find_spec("datacontract.imports.jsonschema_importer"):
-    from datacontract.imports.jsonschema_importer import JsonSchemaImporter
-
-    importer_factory.register_importer(ImportFormat.jsonschema, JsonSchemaImporter)
-
-if importlib.util.find_spec("datacontract.imports.odcs_importer"):
-    from datacontract.imports.odcs_importer import OdcsImporter
-
-    importer_factory.register_importer(ImportFormat.odcs, OdcsImporter)
-
-
-if importlib.util.find_spec("datacontract.imports.sql_importer"):
-    from datacontract.imports.sql_importer import SqlImporter
-
-    importer_factory.register_importer(ImportFormat.sql, SqlImporter)
-
-
-if importlib.util.find_spec("datacontract.imports.sql_importer"):
-    from datacontract.imports.unity_importer import UnityImporter
-
-    importer_factory.register_importer(ImportFormat.unity, UnityImporter)
+            return UnityImporter
+        raise ValueError(f"Import format {name} not supported.")
