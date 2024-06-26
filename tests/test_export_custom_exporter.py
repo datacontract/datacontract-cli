70ce2ea1779daa3404c4b524c08d15b2d48f7dda
Simon Harrer <simon.harrer@gmail.com>
2024-06-26 13:03:55 +0200

    Fix tests

--- tests/test_export_custom_exporter.py
+++ tests/test_export_custom_exporter.py
@@ -23,8 +23,9 @@ exporter_factory.register_exporter("custom", CustomExporter)
 
 
 def test_custom_exporter():
-    expected_custom = """{'data_contract_servers': {'production': Server(type='snowflake', format=None, project=None, dataset=None, path=None, delimiter=None, endpointUrl=None, location=None, account='my-account', database='my-database', schema_='my-schema', host=None, port=None, catalog=None, topic=None, http_path=None, token=None, dataProductId=None, outputPortId=None, driver=None)}, 'model': 'orders', 'server': 'production', 'sql_server_type': 'auto', 'export_args': {'server': 'production', 'custom_arg': 'my_custom_arg'}, 'custom_args': 'my_custom_arg'}"""
+    expected_custom = """{'data_contract_servers': {'production': Server(type='snowflake', description=None, environment=None, format=None, project=None, dataset=None, path=None, delimiter=None, endpointUrl=None, location=None, account='my-account', database='my-database', schema_='my-schema', host=None, port=None, catalog=None, topic=None, http_path=None, token=None, dataProductId=None, outputPortId=None, driver=None)}, 'model': 'orders', 'server': 'production', 'sql_server_type': 'auto', 'export_args': {'server': 'production', 'custom_arg': 'my_custom_arg'}, 'custom_args': 'my_custom_arg'}"""
     result = DataContract(data_contract_file="./fixtures/export/datacontract.yaml", server="production").export(
         export_format="custom", model="orders", server="production", custom_arg="my_custom_arg"
     )
+    # TODO use json comparison instead of string comparison
     assert result.strip() == expected_custom.strip()
