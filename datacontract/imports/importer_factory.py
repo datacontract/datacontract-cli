4d5aa83e7549e4c782e3e448ffc56e82e4a4df00
Carneiro, Rodrigo <Rodrigo.Carneiro@br.experian.com>
2024-06-25 22:27:56 -0300

    fix: load only selected importer

--- datacontract/imports/importer_factory.py
+++ datacontract/imports/importer_factory.py
@@ -6,17 +6,28 @@ from datacontract.imports.importer import ImportFormat, Importer
 class ImporterFactory:
     def __init__(self):
         self.dict_importer = {}
+        self.dict_lazy_importer = {}
 
-    def register_importer(self, name, exporter):
-        self.dict_importer.update({name: exporter})
+    def register_importer(self, name, importer: Importer):
+        self.dict_importer.update({name: importer})
+
+    def register_lazy_importer(self, name: str, module_path: str, class_name: str):
+        self.dict_lazy_importer.update({name: (module_path, class_name)})
 
     def create(self, name) -> Importer:
-        if name not in self.dict_importer.keys():
+        importers = self.dict_importer.copy()
+        importers.update(self.dict_lazy_importer.copy())
+        if name not in importers.keys():
             raise ValueError(f"The '{name}' format is not suportted.")
-        return self.dict_importer[name](name)
+        importer_class = importers[name]
+        if type(importers[name]) == tuple:
+            importer_class = load_module_class(module_path=importers[name][0], class_name=importers[name][1])
+        if not importer_class:
+            raise ValueError(f"Module {name} could not be loaded.")
+        return importer_class(name)
 
 
-def lazy_module_import(module_path):
+def import_module(module_path):
     if importlib.util.find_spec(module_path) is not None:
         try:
             module = importlib.import_module(module_path)
@@ -26,35 +37,36 @@ def lazy_module_import(module_path):
         return module
 
 
-def load_importer(import_format, module_path, class_name):
-    module = lazy_module_import(module_path)
-    if module:
-        importer_class = getattr(module, class_name)
-        importer_factory.register_importer(import_format, importer_class)
+def load_module_class(module_path, class_name):
+    module = import_module(module_path)
+    if not module:
+        return None
+    return getattr(module, class_name)
 
 
 importer_factory = ImporterFactory()
-
-load_importer(
-    import_format=ImportFormat.avro, module_path="datacontract.imports.avro_importer", class_name="AvroImporter"
+importer_factory.register_lazy_importer(
+    name=ImportFormat.avro, module_path="datacontract.imports.avro_importer", class_name="AvroImporter"
 )
-load_importer(
-    import_format=ImportFormat.bigquery,
+importer_factory.register_lazy_importer(
+    name=ImportFormat.bigquery,
     module_path="datacontract.imports.bigquery_importer",
     class_name="BigQueryImporter",
 )
-load_importer(
-    import_format=ImportFormat.glue, module_path="datacontract.imports.glue_importer", class_name="GlueImporter"
+importer_factory.register_lazy_importer(
+    name=ImportFormat.glue, module_path="datacontract.imports.glue_importer", class_name="GlueImporter"
 )
-load_importer(
-    import_format=ImportFormat.jsonschema,
+importer_factory.register_lazy_importer(
+    name=ImportFormat.jsonschema,
     module_path="datacontract.imports.jsonschema_importer",
     class_name="JsonSchemaImporter",
 )
-load_importer(
-    import_format=ImportFormat.odcs, module_path="datacontract.imports.odcs_importer", class_name="OdcsImporter"
+importer_factory.register_lazy_importer(
+    name=ImportFormat.odcs, module_path="datacontract.imports.odcs_importer", class_name="OdcsImporter"
 )
-load_importer(import_format=ImportFormat.sql, module_path="datacontract.imports.sql_importer", class_name="SqlImporter")
-load_importer(
-    import_format=ImportFormat.unity, module_path="datacontract.imports.unity_importer", class_name="UnityImporter"
+importer_factory.register_lazy_importer(
+    name=ImportFormat.sql, module_path="datacontract.imports.sql_importer", class_name="SqlImporter"
+)
+importer_factory.register_lazy_importer(
+    name=ImportFormat.unity, module_path="datacontract.imports.unity_importer", class_name="UnityImporter"
 )
