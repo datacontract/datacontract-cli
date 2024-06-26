17c73d71b5a065933182cdeffaf15fdcc4842127
Rodrigo Carneiro <teoria@gmail.com>
2024-06-26 12:16:57 -0300

    fix: load exporters dynamically (#293)
    * fix: load exporters dynamically
    
    * fix: use importlib.import_module
    
    * fix: load only selected exporter
    
    ---------
    
    Co-authored-by: Carneiro, Rodrigo <Rodrigo.Carneiro@br.experian.com>

--- datacontract/export/exporter_factory.py
+++ datacontract/export/exporter_factory.py
@@ -1,54 +1,145 @@
-from datacontract.export.avro_idl_converter import AvroIdlExporter
-from datacontract.export.bigquery_converter import BigQueryExporter
-from datacontract.export.dbml_converter import DbmlExporter
-from datacontract.export.dbt_converter import DbtExporter, DbtSourceExporter, DbtStageExporter
-from datacontract.export.avro_converter import AvroExporter
+import importlib
+import sys
 from datacontract.export.exporter import ExportFormat, Exporter
-from datacontract.export.go_converter import GoExporter
-from datacontract.export.great_expectations_converter import GreateExpectationsExporter
-from datacontract.export.html_export import HtmlExporter
-from datacontract.export.jsonschema_converter import JsonSchemaExporter
-from datacontract.export.odcs_converter import OdcsExporter
-from datacontract.export.protobuf_converter import ProtoBufExporter
-from datacontract.export.pydantic_converter import PydanticExporter
-from datacontract.export.rdf_converter import RdfExporter
-from datacontract.export.sodacl_converter import SodaExporter
-from datacontract.export.spark_converter import SparkExporter
-from datacontract.export.sql_converter import SqlExporter, SqlQueryExporter
-from datacontract.export.terraform_converter import TerraformExporter
 
 
 class ExporterFactory:
     def __init__(self):
         self.dict_exporter = {}
+        self.dict_lazy_exporter = {}
 
-    def register_exporter(self, name, exporter):
+    def register_exporter(self, name: str, exporter: Exporter):
         self.dict_exporter.update({name: exporter})
 
+    def register_lazy_exporter(self, name: str, module_path: str, class_name: str):
+        self.dict_lazy_exporter.update({name: (module_path, class_name)})
+
     def create(self, name) -> Exporter:
-        if name not in self.dict_exporter.keys():
-            raise ValueError(f"Export format {name} not supported.")
-        return self.dict_exporter[name](name)
+        exporters = self.dict_exporter.copy()
+        exporters.update(self.dict_lazy_exporter.copy())
+        if name not in exporters.keys():
+            raise ValueError(f"The '{name}' format is not supported.")
+        exporter_class = exporters[name]
+        if type(exporters[name]) == tuple:
+            exporter_class = load_module_class(module_path=exporters[name][0], class_name=exporters[name][1])
+        if not exporter_class:
+            raise ValueError(f"Module {name} could not be loaded.")
+        return exporter_class(name)
+
+
+def import_module(module_path):
+    if importlib.util.find_spec(module_path) is not None:
+        try:
+            module = importlib.import_module(module_path)
+        except ModuleNotFoundError:
+            return None
+        sys.modules[module_path] = module
+        return module
+
+
+def load_module_class(module_path, class_name):
+    module = import_module(module_path)
+    if not module:
+        return None
+    return getattr(module, class_name)
 
 
 exporter_factory = ExporterFactory()
-exporter_factory.register_exporter(ExportFormat.avro, AvroExporter)
-exporter_factory.register_exporter(ExportFormat.avro_idl, AvroIdlExporter)
-exporter_factory.register_exporter(ExportFormat.bigquery, BigQueryExporter)
-exporter_factory.register_exporter(ExportFormat.dbml, DbmlExporter)
-exporter_factory.register_exporter(ExportFormat.rdf, RdfExporter)
-exporter_factory.register_exporter(ExportFormat.dbt, DbtExporter)
-exporter_factory.register_exporter(ExportFormat.dbt_sources, DbtSourceExporter)
-exporter_factory.register_exporter(ExportFormat.dbt_staging_sql, DbtStageExporter)
-exporter_factory.register_exporter(ExportFormat.jsonschema, JsonSchemaExporter)
-exporter_factory.register_exporter(ExportFormat.odcs, OdcsExporter)
-exporter_factory.register_exporter(ExportFormat.go, GoExporter)
-exporter_factory.register_exporter(ExportFormat.great_expectations, GreateExpectationsExporter)
-exporter_factory.register_exporter(ExportFormat.html, HtmlExporter)
-exporter_factory.register_exporter(ExportFormat.protobuf, ProtoBufExporter)
-exporter_factory.register_exporter(ExportFormat.pydantic_model, PydanticExporter)
-exporter_factory.register_exporter(ExportFormat.sodacl, SodaExporter)
-exporter_factory.register_exporter(ExportFormat.sql, SqlExporter)
-exporter_factory.register_exporter(ExportFormat.sql_query, SqlQueryExporter)
-exporter_factory.register_exporter(ExportFormat.terraform, TerraformExporter)
-exporter_factory.register_exporter(ExportFormat.spark, SparkExporter)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.avro, module_path="datacontract.export.avro_converter", class_name="AvroExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.avro_idl,
+    module_path="datacontract.export.avro_idl_converter",
+    class_name="AvroIdlExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.bigquery,
+    module_path="datacontract.export.bigquery_converter",
+    class_name="BigQueryExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.dbml, module_path="datacontract.export.dbml_converter", class_name="DbmlExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.rdf, module_path="datacontract.export.rdf_converter", class_name="RdfExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.dbt, module_path="datacontract.export.dbt_converter", class_name="DbtExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.dbt_sources,
+    module_path="datacontract.export.dbt_converter",
+    class_name="DbtSourceExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.dbt_staging_sql,
+    module_path="datacontract.export.dbt_converter",
+    class_name="DbtStageExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.jsonschema,
+    module_path="datacontract.export.jsonschema_converter",
+    class_name="JsonSchemaExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.odcs, module_path="datacontract.export.odcs_converter", class_name="OdcsExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.go, module_path="datacontract.export.go_converter", class_name="GoExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.great_expectations,
+    module_path="datacontract.export.great_expectations_converter",
+    class_name="GreateExpectationsExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.html, module_path="datacontract.export.html_export", class_name="HtmlExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.protobuf,
+    module_path="datacontract.export.protobuf_converter",
+    class_name="ProtoBufExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.pydantic_model,
+    module_path="datacontract.export.pydantic_converter",
+    class_name="PydanticExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.sodacl, module_path="datacontract.export.sodacl_converter", class_name="SodaExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.sql, module_path="datacontract.export.sql_converter", class_name="SqlExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.sql_query, module_path="datacontract.export.sql_converter", class_name="SqlQueryExporter"
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.terraform,
+    module_path="datacontract.export.terraform_converter",
+    class_name="TerraformExporter",
+)
+
+exporter_factory.register_lazy_exporter(
+    name=ExportFormat.spark, module_path="datacontract.export.spark_converter", class_name="SparkExporter"
+)
