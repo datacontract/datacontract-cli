aa15381f7a616c17fe83075dc5cf8ec42020cd2e
Cora Ners <cners@follettlearning.com>
2024-08-22 16:37:46 -0400

    add suppport for exporting enums from yaml to avro

--- datacontract/export/avro_converter.py
+++ datacontract/export/avro_converter.py
@@ -42,8 +42,13 @@ def to_avro_field(field, field_name):
         avro_field["doc"] = field.description
     avro_field["type"] = to_avro_type(field, field_name)
 
+    if avro_field["type"] == "enum":
+        avro_field["type"] = {"type": "enum", "name": field.name, "symbols": field.symbols, "default": field.config["avroDefault"]}
+
     if field.config:
-        if "avroDefault" in field.config:
+        if "avroDefault" in field.config and "avroType" in field.config and field.config["avroType"] != "enum":
+            avro_field["default"] = field.config["avroDefault"]
+        elif "avroDefault" in field.config and "avroType" not in field.config:
             avro_field["default"] = field.config["avroDefault"]
 
     return avro_field
