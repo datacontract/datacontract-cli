09a5b28b57883e4016ea87b8b147ee21f9ed94c7
Fredrik Bakken <Fredrik.Bakken@norges-bank.no>
2024-09-20 12:51:52 +0200

    feat: add sql type converter for dataframe server
    Signed-off-by: Fredrik Bakken <Fredrik.Bakken@norges-bank.no>

--- datacontract/export/sql_type_converter.py
+++ datacontract/export/sql_type_converter.py
@@ -7,6 +7,8 @@ def convert_to_sql_type(field: Field, server_type: str) -> str:
         return convert_to_snowflake(field)
     elif server_type == "postgres":
         return convert_type_to_postgres(field)
+    elif server_type == "dataframe":
+        return convert_to_dataframe(field)
     elif server_type == "databricks":
         return convert_to_databricks(field)
     elif server_type == "local" or server_type == "s3":
@@ -105,6 +107,44 @@ def convert_type_to_postgres(field: Field) -> None | str:
         return "bytea"
     if type.lower() in ["array"]:
         return convert_to_sql_type(field.items, "postgres") + "[]"
+    return None
+
+
+# dataframe data types:
+# https://spark.apache.org/docs/latest/sql-ref-datatypes.html
+def convert_to_dataframe(field: Field) -> None | str:
+    type = field.type
+    if type is None:
+        return None
+    if type.lower() in ["string", "varchar", "text"]:
+        return "STRING"
+    if type.lower() in ["timestamp", "timestamp_tz"]:
+        return "TIMESTAMP"
+    if type.lower() in ["timestamp_ntz"]:
+        return "TIMESTAMP_NTZ"
+    if type.lower() in ["date"]:
+        return "DATE"
+    if type.lower() in ["time"]:
+        return "STRING"
+    if type.lower() in ["number", "decimal", "numeric"]:
+        # precision and scale not supported by data contract
+        return "DECIMAL"
+    if type.lower() in ["float"]:
+        return "FLOAT"
+    if type.lower() in ["double"]:
+        return "DOUBLE"
+    if type.lower() in ["integer", "int"]:
+        return "INT"
+    if type.lower() in ["long", "bigint"]:
+        return "BIGINT"
+    if type.lower() in ["boolean"]:
+        return "BOOLEAN"
+    if type.lower() in ["object", "record", "struct"]:
+        return "STRUCT"
+    if type.lower() in ["bytes"]:
+        return "BINARY"
+    if type.lower() in ["array"]:
+        return "ARRAY"
     return None
 
 
