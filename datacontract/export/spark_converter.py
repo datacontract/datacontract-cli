9ae4232ef34f032158256240bda89ec59d0cb5d1
Fredrik Bakken <Fredrik.Bakken@norges-bank.no>
2024-10-06 19:13:08 +0200

    fix: use data contract's default precision value
    Signed-off-by: Fredrik Bakken <Fredrik.Bakken@norges-bank.no>

--- datacontract/export/spark_converter.py
+++ datacontract/export/spark_converter.py
@@ -128,7 +128,7 @@ def to_data_type(field: Field) -> types.DataType:
     if field_type in ["string", "varchar", "text"]:
         return types.StringType()
     if field_type in ["number", "decimal", "numeric"]:
-        precision = field.precision if field.precision is not None else 10
+        precision = field.precision if field.precision is not None else 38
         scale = field.scale if field.scale is not None else 0
         return types.DecimalType(precision=precision, scale=scale)
     if field_type in ["integer", "int"]:
