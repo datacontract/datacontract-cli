a714e04036ab74e7ecb625f182e9e975115c6856
Stefan Negele <mail@stefannegele.de>
2024-10-30 15:20:46 +0100

    Use pytest.raises

--- tests/test_data_contract_specification.py
+++ tests/test_data_contract_specification.py
@@ -1,10 +1,10 @@
-import unittest
 from uuid import uuid4
+
+import pytest
 
 from datacontract.model.data_contract_specification import DataContractSpecification
 
 
-class DataContractSpecificationException(unittest.TestCase):
-    def test_from_file_raises_exception_if_file_does_not_exist(self):
-        with self.assertRaises(FileNotFoundError):
-            DataContractSpecification.from_file(f"{uuid4().hex}.yaml")
+def test_from_file_raises_exception_if_file_does_not_exist():
+    with pytest.raises(FileNotFoundError):
+        DataContractSpecification.from_file(f"{uuid4().hex}.yaml")
