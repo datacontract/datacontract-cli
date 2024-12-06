b4c89d020a2c64c25efbcced6086484880d5860f
Yasuhisa Yoshida <syou6162@gmail.com>
2024-12-06 09:58:07 +0900

    add test case for exporting dbt sources for bigquery

--- tests/test_export_dbt_sources.py
+++ tests/test_export_dbt_sources.py
@@ -17,19 +17,28 @@ def test_cli():
     assert result.exit_code == 0
 
 
+def test_cli_bigquery():
+    runner = CliRunner()
+    result = runner.invoke(
+        app, ["export", "./fixtures/dbt/export/datacontract.yaml", "--format", "dbt-sources", "--server", "production"]
+    )
+    print(result.stdout)
+    assert result.exit_code == 0
+
+
 def test_to_dbt_sources():
     data_contract = DataContractSpecification.from_file("fixtures/export/datacontract.yaml")
     expected_dbt_model = """
 version: 2
 sources:
   - name: orders-unit-test
-    description: The orders data contract  
+    description: The orders data contract
     database: my-database
-    schema: my-schema  
+    schema: my-schema
     meta:
       owner: checkout
     tables:
-      - name: orders 
+      - name: orders
         description: The orders model
         columns:
           - name: order_id
@@ -42,7 +51,7 @@ sources:
                   min_value: 8
                   max_value: 10
               - dbt_expectations.expect_column_values_to_match_regex:
-                  regex: ^B[0-9]+$      
+                  regex: ^B[0-9]+$
             meta:
               classification: sensitive
               pii: true
@@ -61,6 +70,60 @@ sources:
             tests:
               - dbt_expectations.dbt_expectations.expect_column_values_to_be_of_type:
                   column_type: TEXT
+              - not_null
+              - accepted_values:
+                  values:
+                    - 'pending'
+                    - 'shipped'
+                    - 'delivered'
+"""
+
+    result = to_dbt_sources_yaml(data_contract, "production")
+
+    assert yaml.safe_load(result) == yaml.safe_load(expected_dbt_model)
+
+
+def test_to_dbt_sources_bigquery():
+    data_contract = DataContractSpecification.from_file("./fixtures/dbt/export/datacontract.yaml")
+    expected_dbt_model = """
+version: 2
+sources:
+  - name: orders-unit-test
+    description: The orders data contract
+    database: my-database
+    schema: my-schema
+    meta:
+      owner: checkout
+    tables:
+      - name: orders
+        description: The orders model
+        columns:
+          - name: order_id
+            data_type: STRING
+            tests:
+              - not_null
+              - unique
+              - dbt_expectations.expect_column_value_lengths_to_be_between:
+                  min_value: 8
+                  max_value: 10
+              - dbt_expectations.expect_column_values_to_match_regex:
+                  regex: ^B[0-9]+$
+            meta:
+              classification: sensitive
+              pii: true
+            tags:
+              - order_id
+          - name: order_total
+            description: The order_total field
+            data_type: NUMERIC
+            tests:
+              - not_null
+              - dbt_expectations.expect_column_values_to_be_between:
+                   min_value: 0
+                   max_value: 1000000
+          - name: order_status
+            data_type: STRING
+            tests:
               - not_null
               - accepted_values:
                   values:
