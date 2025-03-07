bcfbae02d0549b49d66c218d6cecee7f9b7ac7d1
ReshuVishwakarma <70638396+ReshuVishwakarma@users.noreply.github.com>
2025-03-07 17:15:20 +0530

    Update test_import_protobuf.py

--- tests/test_import_protobuf.py
+++ tests/test_import_protobuf.py
@@ -49,10 +49,56 @@ models:
         description: Field price
         type: double
         required: false
+      category:
+        description: Enum field category
+        type: string
+        values:
+          CATEGORY_UNKNOWN: 0
+          CATEGORY_ELECTRONICS: 1
+          CATEGORY_CLOTHING: 2
+          CATEGORY_HOME_APPLIANCES: 3
+        required: false
+      tags:
+        description: Field tags
+        type: string
+        required: false
+      reviews:
+        description: List of Review
+        type: array
+        items:
+          type: object
+          fields:
+            user:
+              description: Field user
+              type: string
+              required: false
+            rating:
+              description: Field rating
+              type: integer
+              required: false
+            comment:
+              description: Field comment
+              type: string
+              required: false
+  Review:
+    description: Details of Review.
+    type: table
+    fields:
+      user:
+        description: Field user
+        type: string
+        required: false
+      rating:
+        description: Field rating
+        type: integer
+        required: false
+      comment:
+        description: Field comment
+        type: string
+        required: false
 """
 
     print("Result", result.to_yaml())
     assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
     # Disable linters so we don't get "missing description" warnings
     assert DataContract(data_contract_str=expected).lint(enabled_linters=set()).has_passed()
-
