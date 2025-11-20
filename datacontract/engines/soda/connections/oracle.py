43b15999ea5de6e343a4fa52f374f2c930dcbaad
Timo Loist <timo.loist@innoq.com>
2025-11-20 13:48:32 +0100

    Adapted Changelog Conditional import only when working with an Oracle DB

--- datacontract/engines/soda/connections/oracle.py
+++ datacontract/engines/soda/connections/oracle.py
@@ -1,12 +1,12 @@
 import os
 
-import oracledb
 import yaml
 
 from datacontract.model.data_contract_specification import Server
 
 
 def initialize_client_and_create_soda_configuration(server: Server) -> str:
+    import oracledb
     soda_config = to_oracle_soda_configuration(server)
 
     oracle_client_dir = os.getenv("DATACONTRACT_ORACLE_CLIENT_DIR")
