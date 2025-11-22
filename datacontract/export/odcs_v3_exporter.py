e7691884a94252686d658697455c864d3fa61810
Robert Altmiller <robert.altmiller@databricks.com>
2025-11-22 16:57:13 -0600

    added support for map logical type since it was missing

--- datacontract/export/odcs_v3_exporter.py
+++ datacontract/export/odcs_v3_exporter.py
@@ -199,10 +199,10 @@ def to_logical_type(type: str) -> str | None:
         return "boolean"
     if type.lower() in ["object", "record", "struct"]:
         return "object"
-    if type.lower() in ["bytes"]:
+    if type.lower() in ["bytes", "array"]:
         return "array"
-    if type.lower() in ["array"]:
-        return "array"
+    if type.lower() in ["map"]:
+        return "map"
     if type.lower() in ["variant"]:
         return "variant"
     if type.lower() in ["null"]:
